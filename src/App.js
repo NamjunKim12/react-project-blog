6589b9128619a27c07648a4fcb9464db79b1cc59
Namjun Kim <wrasf175@naver.com>
2021-07-19 19:07:57 +0900

    react project-Modal UI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','신림역 감성카페']);
   let [따봉, 따봉변경 ] = useState(0);
-  let posts = '강남 고기 맛집';
+  let [ 모달, 모달변경 ] = useState(false);
 
   function 제목변경(){
       var newArray = [...글제목];
@@ -20,6 +20,11 @@ function App() {
       }
       글제목변경( newArray );
       }
+
+    function 모달클릭(){
+      모달 == true ? false : true;
+      모달변경( false )
+    }
 
   function 초기화(){
     var newArray = [...글제목];
@@ -50,28 +55,26 @@ function App() {
       <button onClick={ 글순서역순변경 }>제목오름차순 정렬 </button>
 
       <div className="list">
-        <h3>{ 글제목[0] }<span onClick={()=>{따봉변경(따봉 + 1) } } >👍</span> { 따봉 }</h3>
+        <h3 onClick={ ()=>{ 모달변경(!모달) } }>{ 글제목[0] }<span onClick={()=>{따봉변경(따봉 + 1) } } >👍</span> { 따봉 }</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목[1] }<span onClick={()=>{따봉변경(따봉 + 1) } } >👍</span> { 따봉 }</h3>
+        <h3 onClick={ ()=>{ 모달변경(!모달) } }>{ 글제목[1] }<span onClick={()=>{따봉변경(따봉 + 1) } } >👍</span> { 따봉 }</h3>
         <p>2월 19일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목[2] }<span onClick={()=>{따봉변경(따봉 + 1 )} } >👍</span>{ 따봉 }</h3>
+        <h3 onClick={ ()=>{ 모달변경(!모달) } }>{ 글제목[2] }<span onClick={()=>{따봉변경(따봉 + 1 )} } >👍</span>{ 따봉 }</h3>
         <p>2월 24일 발행</p>
         <hr/>
       </div>
 
-      <div className="modal">
-        <h2>제목</h2>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
-
-      <Modal />
+      { 
+         모달 === true 
+         ? <Modal />
+         : null
+      }
     </div>
 
 
